13e30fe6af93bbb48a4795ee22f4f3ba760adc14
Kai Yuan <kent.yuan@Gmail.com>
2013-11-14 00:47:42 +0100

    add get_session_names

--- tmuxback/tmux.py
+++ tmuxback/tmux.py
@@ -1,4 +1,20 @@
 # -*- coding:utf-8 -*-
+import subprocess
+import re
+
+
+#tmux commands
+
+#list sessions
+CMD_LIST_SESSIONS='tmux list-sessions -F#S'
+
+
+
 def get_session_names():
-    """get session names"""
-    pass
+    """ return a list of tmux session names """
+    s = subprocess.check_output(CMD_LIST_SESSIONS.split(' '))
+    s = re.sub('\n$','',s)
+    return s.split('\n')
+
+#if __name__ == '__main__':
+#    print get_session_names()
