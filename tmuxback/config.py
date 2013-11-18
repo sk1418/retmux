fc8ff36b8543cddda2b0b2382e05009fd51a5c79
Kai Yuan <kent.yuan@Gmail.com>
2013-11-19 00:55:40 +0100

    config handling

--- tmuxback/config.py
+++ tmuxback/config.py
@@ -6,6 +6,7 @@ import ConfigParser
 VERSION     = '1.0.0'                #software version
 APP_PATH    = path.join(path.dirname(__file__),"../")
 USER_PATH   = path.join(os.getenv("HOME") , ".tmuxback")
+SAMPLE_CONF    = path.join(APP_PATH, 'sample','default.conf')
 BACKUP_PATH = path.join(USER_PATH, "backup")
 CONF_FILE   = path.join(USER_PATH, "tmuxback.conf")
 
@@ -18,22 +19,18 @@ def update_config(section, key, value):
     with open(CONF_FILE, 'w') as cfgf:
         cf.write(cfgf)
 
-def init_needed():
-    """
-    check if the home config directory exists
-    """
-    return not path.exists(USER_PATH)
 
-def loadConfig():
+
+def load_config():
     """
         load config from config file 
         return True if sucessful, otherwise False
     """
     cf = ConfigParser.ConfigParser()
     
-    # if conf file doesn't exist, return False
+    # if conf file doesn't exist, cp default conf there
     if not path.exists(CONF_FILE):
-        return False
+        __init_config()
 
     cf.read(CONF_FILE);
 
@@ -41,3 +38,12 @@ def loadConfig():
     #TODO
     return True;
 
+def __init_config():
+    """
+    create .tmuxback under home
+    """
+    print "run the tmuxback 1st time. create .tmuxback under your $HOME"
+    #mkdir and copy files
+    os.makedirs(BACKUP_PATH)
+    shutil.copy(SAMPLE_CONF,USER_PATH)
+    print "configuration files are initialized"
