e0d5ae5721b85533dfd5e1e7450122508150a998
Kai Yuan <kent.yuan@Gmail.com>
2013-11-20 01:01:44 +0100

    create session cmd

--- tmuxback/tmux_cmd.py
+++ tmuxback/tmux_cmd.py
@@ -8,11 +8,11 @@ import util
 
 #list sessions
 CMD_LIST_SESSIONS='tmux list-sessions -F#S'
-CMD_LIST_WINDOWS='tmux list-windows -F#{window_index}:#{window_name} -t%s'
+CMD_LIST_WINDOWS='tmux list-windows -F#{window_index}:#{window_name} -t(sess_name)s'
 #tmux list-panes -t {session}:{windowIdx}
 CMD_LIST_PANES='tmux list-panes -t%s:%s -F#{window_index}:#{window_name}'
 
-
+CMD_CREATE_SESSION='tmux new-session -d -s%s -n%s -x%d -y%d'
 
 def get_session_names():
     """ return a list of tmux session names """
@@ -29,13 +29,18 @@ def get_windows_from_session(sess_name):
     
 def get_panes_from_sess_win(sess_name,win_idx):
     """return list of result string"""
-    cmd = (CMD_LIST_PANES % sess_name, win_idx).split(' ')
+    #dict parameter
+    p = (sess_name,win_idx)
+    cmd = (CMD_LIST_PANES % p).split(' ')
     s = util.exec_cmd(cmd)
     return s.split('\n')
 
-
+def create_session(sess_name,win_name,width,height):
+    p = (sess_name,win_name,width,height)
+    cmd = (CMD_CREATE_SESSION % p).split(' ')
+    s = util.exec_cmd(cmd)
 
 
 
 if __name__ == '__main__':
-    print get_windows_from_session('TEST2')
+    pass
