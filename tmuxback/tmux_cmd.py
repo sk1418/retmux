4c3741d0b011faacbbbe38da01be1c051703aa01
Kai Yuan <kent.yuan@Gmail.com>
2013-11-22 23:56:21 +0100

    add renumber win

--- tmuxback/tmux_cmd.py
+++ tmuxback/tmux_cmd.py
@@ -13,6 +13,7 @@ CMD_LIST_WINDOWS='tmux list-windows -F#{window_index}:=:#{window_name}:=:#{windo
 CMD_LIST_PANES = 'tmux list-panes -t%s:%s -F#{pane_index}:=:(#{pane_width},#{pane_height}):=:#{pane_current_path}:=:#{pane_active}'
 
 CMD_CREATE_SESSION='tmux new-session -d -s%s -n%s -x%d -y%d'
+CMD_MOVE_WINDOW='tmux move-window -s%s -t%s'
 
 #capture pane content and save in given file. the first %s is paneIdstr, 2nd %s is filename
 CMD_CAPTURE_PANE='tmux capture-pane -ep -t%s'
@@ -58,6 +59,15 @@ def create_session(sess_name,win_name,width,height):
     p = (sess_name,win_name,width,height)
     cmd = (CMD_CREATE_SESSION % p).split(' ')
     s = util.exec_cmd(cmd)
+
+def renumber_window(sess_name, win_id_from, win_id_to):
+    """
+    renumber the window in session
+    """
+    p = (sess_name  + ':' + str(win_id_from), \
+        sess_name + ':' + win_id_to)
+    cmd = (CMD_MOVE_WINDOW % p).split(' ')
+    util.exec_cmd(cmd)
 
 
 
