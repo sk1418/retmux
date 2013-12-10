38208deb235b86baec348b866fb1c6ba476209bc
Kai Yuan <kent.yuan@Gmail.com>
2013-12-10 10:58:55 +0100

    add some HL for help msg

--- tmuxbk/config.py
+++ tmuxbk/config.py
@@ -16,12 +16,13 @@ CONF_FILE   = path.join(USER_PATH, "retmux.conf")
 
 ######user config####
 WITH_CNT = None
+CNT_WITH_ESC = True
 LOG_LVL_FILE = 'INFO'
 LOG_LVL_CONSOLE = 'INFO'
 
 
 def load_config():
-    global LOG_LVL_FILE, LOG_LVL_CONSOLE
+    global LOG_LVL_FILE, LOG_LVL_CONSOLE, CNT_WITH_ESC
     """
         load config from config file 
         return True if sucessful, otherwise False
@@ -35,19 +36,23 @@ def load_config():
     cf.read(CONF_FILE);
 
     #load options here
-    
-    lvl_file = cf.get('settings','log.level.file')
-    lvl_console = cf.get('settings','log.level.console')
+    try:
+        lvl_file = cf.get('settings','log.level.file')
+        lvl_console = cf.get('settings','log.level.console')
+        CNT_WITH_ESC = cf.getboolean('settings','content.with.escape')
 
-    if lvl_file.lower() not in log.LVL_DICT.keys():
-        print "cannot load log.level.file config, use default: %s" % LOG_LVL_FILE
-    else:
-        LOG_LVL_FILE = lvl_file
+        if lvl_file.lower() not in log.LVL_DICT.keys():
+            print "cannot load log.level.file config, use default: %s" % LOG_LVL_FILE
+        else:
+            LOG_LVL_FILE = lvl_file
 
-    if lvl_console.lower() not in log.LVL_DICT.keys():
-        print "cannot load log.level.console config, use default: %s" %LOG_LVL_CONSOLE
-    else:
-        LOG_LVL_CONSOLE = lvl_console
+        if lvl_console.lower() not in log.LVL_DICT.keys():
+            print "cannot load log.level.console config, use default: %s" %LOG_LVL_CONSOLE
+        else:
+            LOG_LVL_CONSOLE = lvl_console
+    except:
+        print "Warning: Error occured when loading config, using all default values"
+        return False
 
 
     return True;
