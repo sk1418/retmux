434be48d8f4e25c08362204b8f3d752c085ebf1d
Kai Yuan <kent.yuan@Gmail.com>
2013-12-10 23:58:09 +0100

    add license file and setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import glob
+setup(
+    name = 'retmux',
+    version = '1.0.0',
+    packages = find_packages(),
+    data_files=[('conf',glob.glob('conf/*.*'))],
+    include_package_data= True,
+    zip_safe=False,
+    scripts=['retmux'],
+    author='Kai Yuan',
+    author_email='kent.yuan@gmail.com',
+    platforms=['POSIX'],
+    keywords='tmux restore backup',
+    url='https://github.com/sk1418/retmux',
+    description='A tmux session backup and restore tool',
+    long_description="""
+    A tmux session backup and restore tool
+    """,
+)
+
