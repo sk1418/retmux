75e1129d6f3cc9b89d922e629f0c613ffa84fc2c
Kent@LsyHP <kent.yuan@Gmail.com>
2014-10-01 16:22:17 +0200

    add some test str

--- tmuxbk/config.py
+++ tmuxbk/config.py
@@ -5,7 +5,7 @@ import ConfigParser
 import log
 
 
-VERSION     = '1.0.1'                #software version
+VERSION     = '1.0.1_bleeding'                #software version
 #shell command separator
 CMD_SEP     = u'\x97'
 #the separator of tmux command output
